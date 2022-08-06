3b244df5450170c5c5fc619375c6c16c3b0e5aee
Enzo Ferey <hello@enzoferey.com>
2022-08-06 13:10:42 +0200

    Add repo structure.

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+/// <reference types="vitest" />
+import path from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "lib/index.ts"),
+      name: "pending-transactions",
+      fileName: "pending-transactions",
+    },
+  },
+  plugins: [dts()],
+  test: {
+    mockReset: true,
+    coverage: {
+      src: ["lib"],
+      reporter: ["text", "lcov"],
+      all: true,
+      exclude: ["lib/**/index.ts", "lib/types.ts", "*.d.ts"],
+      100: true,
+    },
+  },
+});
